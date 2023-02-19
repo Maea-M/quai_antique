d795a3de7e36000c571b99da9e8f3a1c8860c626
Morgane Mahé <maeamahe@gmail.com>
2023-02-19 10:32:02 +0100

    add entity and create table in database

---
+++ src/Entity/Booking.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+class Booking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $number_guest = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $hour = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumberGuest(): ?int
+    {
+        return $this->number_guest;
+    }
+
+    public function setNumberGuest(int $number_guest): self
+    {
+        $this->number_guest = $number_guest;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHour(): ?\DateTimeInterface
+    {
+        return $this->hour;
+    }
+
+    public function setHour(\DateTimeInterface $hour): self
+    {
+        $this->hour = $hour;
+
+        return $this;
+    }
+}
