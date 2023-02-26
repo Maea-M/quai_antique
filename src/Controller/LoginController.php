aa976aa3261a7ac3375045e871f5e8738cd7bc86
Morgane Mahé <maeamahe@gmail.com>
2023-02-26 09:50:31 +0100

    fix connection between login and bdd

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,20 +9,18 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/login', name: 'login')] 
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        
+    
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
-            'error'         => $error,
-            ]);
+            'error' => $error,
+        ]);
     }
-
+    
     #[Route('/logout', name: 'logout')]
     public function logout(): void
     {
