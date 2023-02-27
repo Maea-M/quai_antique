f2c33cdb0313fb4af3ffdbba0087669a4cc668aa
Morgane Mahé <maeamahe@gmail.com>
2023-02-27 13:24:14 +0100

    add crud for galley image, add mimeType

---
+++ src/Form/ImageGalleryType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ImageGallery;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ImageGalleryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('image', FileType::class, [
+                'label' => 'Image de la recette',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '4096k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Seuls les Jpeg, Jpg et Png sont acceptés',
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ImageGallery::class,
+        ]);
+    }
+}
