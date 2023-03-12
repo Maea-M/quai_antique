829da5c01b0b6cc371536f33c543ef2558ce1439
Morgane Mahé <maeamahe@gmail.com>
2023-03-12 08:18:08 +0100

    bug in imageGallery

--- src/Controller/ImageGalleryController.php
+++ src/Controller/ImageGalleryController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\ImageGallery;
+
+use App\Repository\ImageGalleryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ImageGalleyRepository;
 
 class ImageGalleryController extends AbstractController
 {
     #[Route('/imagegallery', name: 'image_gallery')]
-    public function index(): Response
+
+    public function index(ImageGalleryRepository $imageGalleryRepository): Response
     {
+        $imageGalleries = $imageGalleryRepository->findAll();
         return $this->render('image_gallery/index.html.twig', [
-            'controller_name' => 'ImageGalleryController',
         ]);
     }
 }
