01816b8a9d044c1c9e8ce5794bafb958a0ef1b6a
Morgane Mahé <maeamahe@gmail.com>
2023-03-15 15:21:42 +0100

    add function eye for password

---
+++ public/scripts/functioneye.js
@@ -0,0 +1,17 @@
+/*Sélectionner les éléments*/
+const eye = document.querySelector(".fa-eye");
+const eyeoff = document.querySelector(".fa-eye-slash");
+const passwordField = document.getElementById("#password");
+
+/*faire un addEventListenner, astuce changer le type assword en type text*/
+eye.addEventListener("click", () => {
+    eye.style.display = "none";
+    eyeoff.style.display = "block";
+    passwordField.type = "text";
+});
+
+eyeoff.addEventListener("click", () => {
+    eyeoff.style.display = "none";
+    eye.style.display = "block";
+    passwordField.type = "password";
+});
