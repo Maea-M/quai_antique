180d020cea5625babe52f4a1e15a511244fdf38f
Morgane Mahé <maeamahe@gmail.com>
2023-03-15 20:37:09 +0100

    function password visible works in js

--- public/scripts/functioneye.js
+++ public/scripts/functioneye.js
@@ -1,17 +1,20 @@
-/*Sélectionner les éléments*/
-const eye = document.querySelector(".fa-eye");
-const eyeoff = document.querySelector(".fa-eye-slash");
-const passwordField = document.querySelector("#password");
+// doc symfony :https://symfony.com/doc/current/frontend/encore/server-data.html
+//Sélectionner les éléments
+document.addEventListener('DOMContentLoaded', function() {
+    const eye = document.querySelector('.fa-eye');
+    const eyeOff = document.querySelector('.fa-eye-slash');
+    const passwordField = document.querySelector('#password');
 
-/*faire un addEventListenner, astuce changer le type assword en type text*/
-eye.addEventListener("click", () => {
-    eye.style.display = "none";
-    eyeoff.style.display = "block";
-    passwordField.type = "text";
-});
-
-eyeoff.addEventListener("click", () => {
-    eyeoff.style.display = "none";
-    eye.style.display = "block";
-    passwordField.type = "password";
+    //Ecouter les évènements
+    eye.addEventListener('click', () => {
+        eye.style.display = "none";
+        eyeOff.style.display = "block";
+        passwordField.type = "text";
+    });
+    
+    eyeOff.addEventListener('click', () => {
+        eyeOff.style.display = "none";
+        eye.style.display = "block";
+        passwordField.type = "password";
+    });
 });
