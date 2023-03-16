e934ed507224b87757854c3dc1274d0ba0af4c39
Morgane Mahé <maeamahe@gmail.com>
2023-03-16 21:45:00 +0100

    add css

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,1 +1,23 @@
 console.log("hello world!")
+
+/*Ala 
+var settings = {
+    "url": "http://15.236.148.78/products/all",
+    "method": "GET",
+    "timeout": 0,
+    };
+    
+    $.ajax(settings).done(function (response) {
+    console.log(response);
+    });
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+        };
+        
+        fetch("http://15.236.148.78/products/all", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        */
