1a646acec1c52b950ce6d676b96abd92a8d635d5
Morgane Mahé <maeamahe@gmail.com>
2023-03-18 07:58:02 +0100

    try booking js

--- public/scripts/functionbooking.js
+++ public/scripts/functionbooking.js
@@ -1,31 +1,30 @@
-console.log('hello world!')
-/*
-1.Mettre des id correspondant aux champs à chercher
-- date
-- heure
-- nombre de couverts
-- bouton d'envoi du formulaire?
-*/
 document.addEventListener('DOMContentLoaded', function() {
-    const dateTime = document.querySelector("#booking_form_date")
-    console.log(dateTime)
+
+    document.getElementById("btnsubmit").addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        var data = new FormData(this);
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                var res = this.response;
+                if (res.success) {
+                    console.log("Votre réservation a bien été prise en compte");
+                } else {
+                    alert(res.msg);
+                }
+            } else if (this.readyState == 4) {
+                alert("Une erreur est survenue...");
+            }
+        };
+
+    xhr.open("POST", "/booking", true);
+    xhr.responseType = "json";
+    xhr.send(data);
+
+    return false;
+    })
 })
-
-
-//2. commencer le tout à 0, mettre un max par exemple 100
-dateTime[0].value = "";
-//3. Faire un appel pour voir le nombre de places disponibles
-setInterval(function () { getNumberGuests(dateTimeInput[0].value); }, 100);
-
-function getNumberGuests(dateTime) {
-    //mettre ajax({
-//    url : '/booking',
-//    type: 'POST',
-//    data : {'dateTime': dateTime },
-//})
-}
-
-/*4. Si pas de créneaux => mettre un message : pas de dispo
-5. Si crénaux dispo, valider la réservation => mettre un message : résa effectuée
-*/
-
